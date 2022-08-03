34fe4834fef1ae2d917c0a46f83584a8834cb4e0
shuangcherry <2718407083@qq.com>
2022-08-03 10:19:01 +0800

    feat: 完整的promise方法

--- promise/mypromise.js
+++ promise/mypromise.js
@@ -5,7 +5,6 @@ const PENDING = 'pending';
 const FULFILLED = 'fulfilled';
 const REJECTED = 'reject';
 
-
 class Promise{
     value = null;
     state = PENDING;
@@ -16,14 +15,18 @@ class Promise{
             if(this.state === PENDING){
                 this.state = FULFILLED;
                 this.value = val;
-                this.onFulfilled.forEach(item => item(this.value));
+                setTimeout(() => {
+                    this.onFulfilled.forEach(item => item(this.value));
+                },0)
             }
         }
         let reject = (val) => {
             if(this.state === PENDING){
                 this.state = REJECTED;
                 this.value = val;
-                this.onRejected.forEach(item => item(this.value));
+                setTimeout(() => {
+                    this.onRejected.forEach(item => item(this.value));
+                },0)
             }
         }
         try{
@@ -44,7 +47,7 @@ class Promise{
             let callback = (fn) => {
                 try{
                     let result = fn(this.value);
-                    if(typeof result === 'promise'){
+                    if(result instanceof Promise){
                         result.then(res => resolve(res), err => reject(err))
                     }else{
                         resolve(result);
@@ -54,9 +57,9 @@ class Promise{
                 }
             }
             if(this.state === FULFILLED){
-                callback(onFulfilled);
+                setTimeout(callback, 0, onFulfilled);
             }else if(this.state === REJECTED){
-                callback(onRejected);
+                setTimeout(callback, 0, onRejected);
             }else{
                 this.onFulfilled.push(() => {
                     callback(onFulfilled);
@@ -71,16 +74,100 @@ class Promise{
         return this.then(undefined, onFulfilled);
     }
 
-    static resolve(){
-
+    finally(onFinally){
+        return new Promise((resolve, reject) => {
+            let callback = () => {
+                try{
+                    onFinally();
+                    if(this.state === REJECTED){
+                        reject(this.value);
+                    }else if(this.state === FULFILLED){
+                        resolve(this.value);
+                    }
+                }catch(err){
+                    reject(this.value);
+                }
+            }
+            if(this.state !== PENDING){
+                this.onFulfilled.push(callback);
+                this.onRejected.push(callback);
+            }else{
+                setTimeout(callback, 0);
+            }
+        })
     }
 
-    static all(){
-
+    static resolve(val){
+        return new Promise((resolve, reject) => {
+            if(val instanceof Promise){
+                val.then(res => {
+                    resolve(res);
+                }, err => {
+                    reject(err);
+                })
+            }else{
+                resolve(val);
+            }
+        })
     }
 
-    static race(){
-        
+
+    static reject(val){
+        return new Promise((resolve, reject) => {
+            reject(val);
+        })
+    }
+
+    static all(promiseArr){
+        let resultArr = [];
+        let count = 0;
+        return new Promise((resolve, reject) => {
+            for(let i = 0; i < promiseArr.length; i++){
+                Promise.resolve(promiseArr[i]).then(res => {
+                    count++;
+                    resultArr[i] = res;
+                    if(count === promiseArr.length){
+                        resolve(resultArr);
+                    }
+                }, err => {
+                    reject(err);
+                })
+            }
+        })
+    }
+
+    static race(promiseArr){
+        return new Promise((resolve, reject) => {
+            for(let i = 0; i < promiseArr.length; i++){
+                Promise.resolve(promiseArr[i]).then(res => {
+                    resolve(res);
+                }, err => {
+                    reject(err);
+                })
+            }
+        })
+    }
+
+    static any(promiseArr){
+        let count = 0;
+        let errArr = [];
+        return new Promise((resolve, reject) => {
+            if(promiseArr.length === 0){
+                reject(new AggregateError(errArr, 'All promises were rejected'))
+            }
+            for(let i = 0; i < promiseArr.length; i++){
+                Promise.resolve(promiseArr[i]).then(res => {
+                    resolve(res);
+                }).catch(err => {
+                    count++;
+                    errArr[i] = err;
+                    if(count === promiseArr.length){
+                        reject(new AggregateError(errArr, 'All promises were rejected'))
+                    }
+                })
+            }
+
+        })
     }
 }
 
